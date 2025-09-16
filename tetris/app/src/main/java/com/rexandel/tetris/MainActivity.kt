713c1d77fff02eb43e8134118e85c26e03dabd56
rexandel <xarendel@gmail.com>
2025-09-17 00:33:23 +0300

    Task 1.1 (Added basic layouts)

--- tetris/app/src/main/java/com/rexandel/tetris/MainActivity.kt
+++ tetris/app/src/main/java/com/rexandel/tetris/MainActivity.kt
@@ -5,16 +5,46 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.text.replace
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNavigation.selectedItemId = R.id.home_page
+
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.skins -> {
+                    replaceFragment(SkinsFragment())
+                    true
+                }
+                R.id.home_page -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.tournament -> {
+                    replaceFragment(TournamentFragment())
+                    true
+                }
+                else -> false
+            }
         }
+
+        if (savedInstanceState == null) {
+            replaceFragment(HomeFragment())
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
---
+++ tetris/app/src/main/java/com/rexandel/tetris/SkinsFragment.kt
@@ -0,0 +1,16 @@
+package com.rexandel.tetris
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SkinsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_skins, container, false)
+    }
+}
